e8e7cd7900354a1dfdee18d735890187f3dd812b
Kaung Si Thu <kst6504250014@gmail.com>
2023-11-19 16:24:48 -0800

    better fetching counts

--- get_apis.js
+++ get_apis.js
@@ -1,4 +1,7 @@
-fetch("https://oogpwbkj6g.execute-api.us-west-1.amazonaws.com/Prod/put_count")
+fetch("https://oogpwbkj6g.execute-api.us-west-1.amazonaws.com/Prod/get_count")
+    .then(data => data.json())
+    .then(data => document.getElementById('count').innerHTML = data['count'])
+    .then(fetch("https://oogpwbkj6g.execute-api.us-west-1.amazonaws.com/Prod/put_count"))
     .then(data => fetch("https://oogpwbkj6g.execute-api.us-west-1.amazonaws.com/Prod/get_count"))
     .then(data => data.json())
     .then(data => document.getElementById('count').innerHTML = data['count'])
