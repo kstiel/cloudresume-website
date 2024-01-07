c62d5609bc433e84f6e6fa4e77da2a2e81350e8f
Kaung Si Thu <kst6504250014@gmail.com>
2024-01-07 14:59:53 -0800

    a few updates

--- get_apis.js
+++ get_apis.js
@@ -1,10 +1,3 @@
-// fetch("https://oogpwbkj6g.execute-api.us-west-1.amazonaws.com/Prod/get_count")
-//     .then(data => data.json())
-//     .then(data => document.getElementById('count').innerHTML = data['count'])
-//     .then(fetch("https://oogpwbkj6g.execute-api.us-west-1.amazonaws.com/Prod/put_count"))
-//     .then(data => fetch("https://oogpwbkj6g.execute-api.us-west-1.amazonaws.com/Prod/get_count"))
-//     .then(data => data.json())
-//     .then(data => document.getElementById('count').innerHTML = data['count'])
 
 webSocket = new WebSocket("wss://gcj1z4m8ta.execute-api.us-west-1.amazonaws.com/depolyment/");
 
